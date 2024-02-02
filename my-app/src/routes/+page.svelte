6b66598e0d10092ccef9715c9a10808f916b4047
Bourourou <136940252+bourourou2301@users.noreply.github.com>
2024-02-02 11:09:24 -0500

    commit

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -3,3 +3,20 @@
 </style>
 
 <h1 class="titre"> ECCIP </h1>
+
+
+<!--
+<div class="sidebar">
+    <ul>
+        <a href=""></a>
+    </ul>
+</div>
+-->
+
+<div>
+    <nav class="navbar">
+        <a href="https://www.youtube.com">Accueil</a>
+        <a href="https://www.youtube.com">Feed</a>
+        <a href="https://www.youtube.com">A propos</a>
+    </nav>
+</div>
