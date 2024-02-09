c8cc42fb69da011c649b7f8770c279bdfe6db62e
Bourourou <136940252+bourourou2301@users.noreply.github.com>
2024-02-09 09:01:43 -0500

    Recreer projet

---
+++ eccip-app/src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<style>
+    @import './style.css';
+</style>
+
+<h1 class="titre"> ECCIP </h1>
+
+
+<!--
+<div class="sidebar">
+    <ul>
+        <a href=""></a>
+    </ul>
+</div>
+-->
+
+<div>
+    <nav class="navbar">
+        <a href="https://www.youtube.com">Accueil</a>
+        <a href="https://www.youtube.com">Feed</a>
+        <a href="https://www.youtube.com">A propos</a>
+    </nav>
+</div>
