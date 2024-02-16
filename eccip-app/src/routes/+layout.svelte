00d06b03aeed92a668073cbd6e14da735f8933e8
Bourourou <136940252+bourourou2301@users.noreply.github.com>
2024-02-16 10:49:57 -0500

    Creer layout navbar

---
+++ eccip-app/src/routes/+layout.svelte
@@ -0,0 +1,73 @@
+<script>
+    import firebase from '$lib/firebase';
+    import { set, ref } from 'firebase/database';
+    var counter = 0;
+    function writeToDatabase() {
+        const database = firebase.database;
+        const dataToWrite = {
+            TimeAtTimeOfButtonPress : new Date().toLocaleString()
+        };
+    const dbRef = ref(database, 'TimeAtButtonPress/PressNumber'+counter+'/');
+    counter = counter +1;
+    set(dbRef, dataToWrite);
+    }
+</script>
+
+<div id="navbar-parent">
+        <nav class="navbar">
+            <a href="">Accueil</a>
+            <a href="">Chat</a>
+            <a href="">Horaire</a>
+            <a href="">À propos</a>
+        </nav>
+        <div id="profile-pic"></div>
+        <button on:click={writeToDatabase}>Write To Database</button>
+</div>
+<slot></slot>
+
+<style>
+#navbar-parent {
+    background-color: rgb(62, 62, 155);
+    width: 150px;
+    height: 80vh;
+    box-shadow: 10px 10px 10px #5e5454;
+    padding: 20px;
+    margin-left: 20px;
+    margin-top: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: fixed;
+    border-radius: 20px;
+}
+
+.navbar {
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    box-sizing: border-box;
+    width:fit-content;
+    height: fit-content;
+    justify-content: center;
+    align-items: center;
+}
+
+.navbar a {
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+        color: rgb(0, 0, 0);
+    text-decoration: none;
+    background-color: transparent;
+}
+
+#profile-pic {
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    background-color: black;
+    border-radius: 50%;
+    width: 65px;
+    height: 65px;
+}
+</style>
--- eccip-app/src/routes/+page.svelte
+++ eccip-app/src/routes/+page.svelte
@@ -1,42 +1,83 @@
-<style>
-    @import './style.css';
-</style>
+<!--PK YA UNE FUCKING ERREUR ICI-->
 
-<h1 class="titre"> ECCIP </h1>
-
-
+<div id="page-entiere">
+    <h1 class="titre"> ECCIP </h1>
 <!--
-<div class="sidebar">
-    <ul>
-        <a href=""></a>
-    </ul>
+<div id="navbar-parent">
+        <nav class="navbar">
+            <a href="">Accueil</a>
+            <a href="">Chat</a>
+            <a href="">Horaire</a>
+            <a href="">À propos</a>
+        </nav>
+        <div id="profile-pic"></div>
+        <button on:click={writeToDatabase}>Write To Database</button>
+    </div>
+-->
 </div>
--->
 
-<!--
-    example of a database set, be sure to import the first line, in the second you add what methods u need (set, update, delete, ect)
--->
-<script>
-import firebase from '$lib/firebase';
-import { set, ref } from 'firebase/database';
-var counter = 0;
-function writeToDatabase() {
-    const database = firebase.database;
-    const dataToWrite = {
-        TimeAtTimeOfButtonPress : new Date().toLocaleString()
-    };
-const dbRef = ref(database, 'TimeAtButtonPress/PressNumber'+counter+'/');
-counter = counter +1;
-set(dbRef, dataToWrite);
+
+
+
+<style>    
+* {
+    margin: 0;
 }
-</script>
 
-<div>
-    <!--Button that writes to db, remove if uneeded-->
-    <button on:click={writeToDatabase}>Write To Database</button>
-    <nav class="navbar">
-        <a href="https://www.youtube.com">Accueil</a>
-        <a href="https://www.youtube.com">Feed</a>
-        <a href="https://www.youtube.com">A propos</a>
-    </nav>
-</div>
+#page-entiere {
+    height: 200vh;
+}
+
+.titre {
+    text-align: center;
+    font-family: 'Courier New', Courier, monospace;
+}
+
+
+/*
+#navbar-parent {
+    background-color: rgb(62, 62, 155);
+    width: 150px;
+    height: 80vh;
+    box-shadow: 10px 10px 10px #5e5454;
+    padding: 20px;
+    margin-left: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    position: fixed;
+    border-radius: 20px;
+}
+
+.navbar {
+    display: flex;
+    gap: 20px;
+    flex-direction: column;
+    box-sizing: border-box;
+    width:fit-content;
+    height: fit-content;
+    justify-content: center;
+    align-items: center;
+}
+
+a {
+    font-family: 'Courier New', Courier, monospace;
+    text-align: center;
+        color: rgb(0, 0, 0);
+    text-decoration: none;
+    background-color: transparent;
+}
+
+#profile-pic {
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 50px;
+    background-color: black;
+    border-radius: 50%;
+    width: 65px;
+    height: 65px;
+}
+*/
+
+</style>
