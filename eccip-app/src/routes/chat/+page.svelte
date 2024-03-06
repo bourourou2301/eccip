cfa14cf574b910c7da22d6e838acdc5a45565147
Zakary7785 <149434486+Zakary7785@users.noreply.github.com>
2024-03-06 14:31:57 -0500

    Progres cadrage de la page horaire

--- eccip-app/src/routes/chat/+page.svelte
+++ eccip-app/src/routes/chat/+page.svelte
@@ -1,3 +1,13 @@
-<div class="container-fluid text-center">
-    <h1>My chats</h1>
+
+<div class="titrePage">
+    <div class="container-fluid text-center">
+        <h1>Mes Messages</h1>
+    </div>
 </div>
+<style>
+    .titrePage{
+        background-color: rgb(62, 62, 155);
+        color: aliceblue;
+    }
+
+</style>
