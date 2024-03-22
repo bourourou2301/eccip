3941c4dae9384205de537d5368ea76f43e26111c
Zakary7785 <149434486+Zakary7785@users.noreply.github.com>
2024-03-22 11:13:56 -0400

    Esthétique de base de la platforme en entier

--- eccip-app/src/routes/offreDemploi/+page.svelte
+++ eccip-app/src/routes/offreDemploi/+page.svelte
@@ -1,52 +1,34 @@
 
 	<script>
 		let typeUser = ''; // la variable pour afficher le type de page correct selon le type de user
+		function pageCréationOffre() { window.location.href = 'offreDemploi/CreerOffre'; }
+		import "$lib/style.css";
 	</script>
 
 
-    <div class="titrePage">
+    <div class="bg-haut">
         <div class="container-fluid text-center">
             <h1>Les offres d'emploi</h1>
         </div>
     </div>
 
-    <div>
-        {#if typeUser != 'recruteur'}
-        <header>
-            <input type="text" id="searchInput" placeholder="Rechercher..." />
+    <div class="contenu-page">
+        
+        <header class="d-flex justify-content-center">
+			<div class= "row">
+			<div class ="recherche">
+            	<input type="text" id="searchInput" placeholder="Rechercher..." />
+			</div>
+			<div class= "boutonPageOffre">
+				<button class="btn btn-primary"on:click= {pageCréationOffre}>créer une offre</button>
+
+			</div>
+		</div>
         </header>
         <main id="jobOffers">
             <h1> Les offres de jobs ici</h1>
         </main>
-        {:else}
-        <!-- version de la page pour les recruteur-->
-        {/if}
+
     </div>
 
 
-<style>
-	.titrePage{
-		background-color:rgb(62, 62, 155) ;
-		color: aliceblue;
-	}
-	
-	header {
-		background-color: #f0f0f0;
-		padding: 20px;
-		text-align: center;
-	}
-
-	#searchInput {
-		padding: 10px;
-		width: 300px;
-		border-radius: 5px;
-		border: 1px solid #ccc;
-	}
-	#jobOffers {
-
-		border: 1px solid #ccc;
-		padding: 20px;
-		margin-bottom: 10px;
-		margin-left: 120px;
-	}
-</style>
