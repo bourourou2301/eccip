cc79a8e327b5531e17fe9e9b548d9cb11142249d
Bourourou <136940252+bourourou2301@users.noreply.github.com>
2024-03-22 08:46:53 -0400

    Creer une debut de chat

--- eccip-app/src/routes/+page.svelte
+++ eccip-app/src/routes/+page.svelte
@@ -1,12 +1,20 @@
+<script>
+    import Message from "$lib/message";
+	import { session } from "$lib/stores/session";
+    const message1 = new Message();
+</script>
+
 <div>
     <h1 style="text-align: center;">BIENVENUE SUR ECCIP</h1>
     <a href="/creer-offre">Creer offre</a>
+    <!-- <button on:click={message1.envoyerMessage(session.subscribe())}>Test message</button> -->
 </div>
 
 <style>
-    a{
+    a, button{
         display: flex;
         justify-content: center;
         align-items: center;
     }
+
 </style>
