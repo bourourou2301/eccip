391e1af4d5211d84a4895bcaed9667bbe2525e31
Zakary7785 <149434486+Zakary7785@users.noreply.github.com>
2024-05-03 12:27:58 -0400

    Esthétique et ajustement de la logique du chat

--- eccip-app/src/routes/chat/+page.svelte
+++ eccip-app/src/routes/chat/+page.svelte
@@ -1,186 +1,174 @@
 <script lang="ts">
-    import firebase from '$lib/firebase';
-    import {session} from "$lib/stores/session";
-    import { collection, onSnapshot, doc, getDoc} from "firebase/firestore"; 
-    import ChatBar from './ChatBar.svelte'; // Chemin relatif à la position du fichier +page.svelte
-    import Message from '$lib/message';
+    import "$lib/style.css";
+	import firebase from '$lib/firebase';
+	import { session } from '$lib/stores/session';
+	import { collection, onSnapshot, doc, getDoc } from 'firebase/firestore';
+	import ChatBar from './ChatBar.svelte'; // Chemin relatif à la position du fichier +page.svelte
+	import Message from '$lib/message';
 	import { onMount } from 'svelte';
-    
-    const db = firebase.db;
+	import { createEventDispatcher } from 'svelte';
 
-    const message = new Message();
+	const dispatch = createEventDispatcher();
 
-    let listeRecipient: Map<string, string> = new Map;
-    let isSnapshotLoaded: boolean = false;
-    let uid: string;
-    let monNom: string;
-    let valeurCle: string;
-    let uidRecipient: string;
-    let uidRecipientChoisi: string;
-    let nomChoisi: string;
-    let afficherPopup: boolean = false;
-    let afficherChatComponent: boolean = false;
-    let listeUtilisateurs: Map<string, string> = new Map();
-    let listeMessages: Map<string, boolean> = new Map();
-    let listeUtilisateursTextees: string[] = []; 
+	const db = firebase.db;
 
-    onMount(async () => {
-        session.subscribe(async (cur: any) => {
-            uid = cur?.sUid.uid;
-            const unsub = onSnapshot(doc(db, "utilisateurs", uid), (doc) => {
-                monNom = doc.get("prenom") + " " + doc.get("nom");
-            });
-            await obtenirAnciensChats();
-            await obtenirListeUtilisateurs();
-        });
-    })
-    
+	const message = new Message();
 
-    async function continuerConvo(event: MouseEvent) {
-        uidRecipient = (event.target as HTMLButtonElement).value;
-        valeurCle = (event.target as HTMLButtonElement).name;
-        afficherChatComponent = true;
-        recuprerMessagesEnvoyes();
-    }
+	let listeRecipient: Map<string, string> = new Map();
+	let isSnapshotLoaded: boolean = false;
+	let uid: string;
+	let monNom: string;
+	let valeurCle: string;
+	let uidRecipient: string;
+	let uidRecipientChoisi: string;
+	let nomChoisi: string;
+	let afficherPopup: boolean = false;
+	let afficherChatComponent: boolean = false;
+	let listeUtilisateurs: Map<string, string> = new Map();
+	let listeMessages: Map<string, boolean> = new Map();
+	let listeUtilisateursTextees: string[] = [];
 
-    async function creerConvo(event: MouseEvent) {
-        nomChoisi = (event.target as HTMLButtonElement).value;
-        uidRecipientChoisi = (event.target as HTMLButtonElement).name;
-        message.creerConversation(uid, uidRecipientChoisi, nomChoisi, monNom);
-    }
+	onMount(async () => {
+		session.subscribe(async (cur: any) => {
+			uid = cur?.sUid.uid;
+			const unsub = onSnapshot(doc(db, 'utilisateurs', uid), (doc) => {
+				monNom = doc.get('prenom') + ' ' + doc.get('nom');
+			});
+			await obtenirAnciensChats();
+			await obtenirListeUtilisateurs();
+		});
+	});
 
-    async function recuprerMessagesEnvoyes() {
-        const unsub = onSnapshot(collection(db, "conversations", valeurCle, "messages"), (collection) => {
-            collection.forEach((doc) => {
-                if (doc.get("envoyePar") === uid) {
-                  listeMessages.set(doc.get("message"), true);
-                } else {
-                  listeMessages.set(doc.get("message"), false)
-                }
-            });
-        });
-    }
+	async function continuerConvo(event: MouseEvent) {
+		listeMessages.clear();
+		uidRecipient = (event.target as HTMLButtonElement).value;
+		valeurCle = (event.target as HTMLButtonElement).name;
+		afficherChatComponent = true;
+		console.log(listeMessages);
 
-//pk ca ecrit undefined kan tu load pour la premiere fois
-async function obtenirAnciensChats() {
-    const unsub = onSnapshot(collection(db, "utilisateurs", uid, "chats"), (collection) => {
-        collection.forEach((doc) => {
-            let isDocumentEmpty: boolean = Object.keys(doc.data() || {}).length === 0;
-            if (!isDocumentEmpty) {
-                listeRecipient.set(doc.get("nomComplet"), doc.get("cleConversation"));
-                listeUtilisateursTextees.push(doc.get("utilisateurRecipient"));
-            }
-        });
-        isSnapshotLoaded = true;
-        listeUtilisateursTextees.forEach(element => {
-            console.log("neg txt: " + element);
-        });
-    });
-}
+		await recuprerMessagesEnvoyes();
+        fermerPopup();
+	}
 
-    async function obtenirListeUtilisateurs() {
-        const unsub = onSnapshot(collection(db, "utilisateurs"), (collection) => {
-            collection.forEach((doc) => {
-                if(uid === doc.get("uid")){
-                    console.log("Tu peux pas t'envoyer des messages a toi meme")
-                } else {
-                    if(listeUtilisateursTextees.length !== 0){
-                        listeUtilisateursTextees.forEach(element => {
-                            if(element === doc.get("uid")) {
-                                console.log("Vous avez deja un convo avec " + element);
-                            } else {
-                                listeUtilisateurs.set(doc.get("uid"), doc.get("prenom") + " " + doc.get("nom"));
-                            }
-                        });
-                    } else {
-                        listeUtilisateurs.set(doc.get("uid"), doc.get("prenom") + " " + doc.get("nom"));
-                    }
-                    
-                }
-            });
-        });
-    }
+	async function creerConvo(event: MouseEvent) {
+		nomChoisi = (event.target as HTMLButtonElement).value;
+		uidRecipientChoisi = (event.target as HTMLButtonElement).name;
+		message.creerConversation(uid, uidRecipientChoisi, nomChoisi, monNom);
+        fermerPopup();
+	}
 
-    async function ouvrirPopUp() {
-        afficherPopup = true;
-    }
-    async function fermerPopup() {
-        afficherPopup = false;
-    }
-    
+	async function recuprerMessagesEnvoyes() {
+		const unsub = onSnapshot(
+			collection(db, 'conversations', valeurCle, 'messages'),
+			(collection) => {
+				collection.forEach((doc) => {
+					if (doc.get('envoyePar') === uid) {
+						listeMessages.set(doc.get('message'), true);
+					} else {
+						listeMessages.set(doc.get('message'), false);
+					}
+				});
+				listeMessages = listeMessages;
+			}
+		);
+	}
+
+	//pk ca ecrit undefined kan tu load pour la premiere fois
+	async function obtenirAnciensChats() {
+		const unsub = onSnapshot(collection(db, 'utilisateurs', uid, 'chats'), (collection) => {
+			collection.forEach((doc) => {
+				let isDocumentEmpty: boolean = Object.keys(doc.data() || {}).length === 0;
+				if (!isDocumentEmpty) {
+					listeRecipient.set(doc.get('nomComplet'), doc.get('cleConversation'));
+					listeUtilisateursTextees.push(doc.get('utilisateurRecipient'));
+				}
+			});
+			isSnapshotLoaded = true;
+		});
+	}
+
+	async function obtenirListeUtilisateurs() {
+		const unsub = onSnapshot(collection(db, 'utilisateurs'), (collection) => {
+			collection.forEach((doc) => {
+				if (uid === doc.get('uid')) {
+					console.log("Tu peux pas t'envoyer des messages a toi meme");
+				} else {
+					if (listeUtilisateursTextees.length !== 0) {
+						listeUtilisateursTextees.forEach((element) => {
+							if (element === doc.get('uid')) {
+								console.log('Vous avez deja un convo avec ' + element);
+							} else {
+								listeUtilisateurs.set(doc.get('uid'), doc.get('prenom') + ' ' + doc.get('nom'));
+							}
+						});
+					} else {
+						listeUtilisateurs.set(doc.get('uid'), doc.get('prenom') + ' ' + doc.get('nom'));
+					}
+				}
+				console.log(listeUtilisateursTextees);
+				listeUtilisateurs = listeUtilisateurs;
+			});
+		});
+	}
+
+	async function ouvrirPopUp() {
+		afficherPopup = true;
+	}
+	async function fermerPopup() {
+		afficherPopup = false;
+	}
 </script>
 
-
 <div class="titrePage">
-    <div class="container-fluid text-center">
-        <h1>Mes Messages</h1>
-    </div>
-</div>
-<div> 
-    <div class="container-fluid contenuPage">
-        <div class="row">
-            <div class="col-md-3 bg-light messageOuvert">
-                <h2>Mes converstations</h2>
-                <button on:click={ouvrirPopUp}>Ajouter conversation</button>
-                {#if afficherPopup}
-                  <div class="popup-container">
-                    <div class="popup">
-                      <h2>Available Users</h2>
-                        {#each listeUtilisateurs as [key, value]}
-                          <button name={key} value={value} on:click={creerConvo}>{value}</button>
-                        {/each}
-                      <button on:click={fermerPopup}>Close</button>
-                    </div>
-                  </div>
-                {/if}
-                {#if isSnapshotLoaded}
-                    {#each listeRecipient as [key, value]}
-                        <button name={value} value={key} on:click={continuerConvo}>{key}</button>
-                    {/each}    
-                {/if}
-
-            </div>
-            <div class="col-md-9 bg-white messageActuel">
-                <h1>Conversation</h1>
-                <!-- <ChatBar utilisateurEnvoyeur = {uid} utilisateurRecipient = {uidRecipient} cleConversation = {valeurCle}/> -->
-                <!-- <p>Ici serait le dernier message ouvert</p> -->
-            </div>
-        </div>
-    </div>
+	<div class="container-fluid text-center">
+		<h1>Mes Messages</h1>
+	</div>
 </div>
 <div>
-    <!-- Contenu de la page +page -->
-    <!-- Utilisation de la barre de chat -->
-    <ChatBar utilisateurEnvoyeur = {uid} utilisateurRecipient = {uidRecipient} cleConversation = {valeurCle} afficherMessage = {afficherChatComponent} listeMessages = {listeMessages}/>
+	<div class="container-fluid contenuPage">
+		<div class="row">
+			<div class="col-md-3 bg-light messageOuvert">
+				<h2>Mes conversations</h2>
+				<button on:click={ouvrirPopUp}>Ajouter conversation</button>
+				{#if afficherPopup}
+					<div class="popup-container">
+						<div class="popup">
+							<h2>Available Users</h2>
+							{#each listeUtilisateurs as [key, value]}
+								<button name={key} {value} on:click={creerConvo}>{value}</button>
+							{/each}
+							<button on:click={fermerPopup}>Close</button>
+						</div>
+					</div>
+				{/if}
+				{#if isSnapshotLoaded}
+					<ul class="conversations-list">
+						{#each listeRecipient as [key, value]}
+							<li class="conversation-item">
+								<button name={value} value={key} on:click={continuerConvo}>{key}</button>
+							</li>
+							<hr class="conversation-separator" />
+						{/each}
+					</ul>
+				{/if}
+			</div>
+			<div class="col-md-9 bg-white messageActuel">
+				<h1>Conversation avec {uidRecipient?uidRecipient:''}</h1>
+				<!-- <p>Ici serait le dernier message ouvert</p> -->
+			</div>
+		</div>
+	</div>
+</div>
+<div>
+	<!-- Contenu de la page +page -->
+	<!-- Utilisation de la barre de chat -->
+	<ChatBar
+		utilisateurEnvoyeur={uid}
+		utilisateurRecipient={uidRecipient}
+		cleConversation={valeurCle}
+		afficherMessage={afficherChatComponent}
+		{listeMessages}
+	/>
 </div>
 
-<style>
-    .popup-container {
-      position: fixed;
-      top: 0;
-      left: 0;
-      width: 100%;
-      height: 100%;
-      background-color: rgba(0, 0, 0, 0.5);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-    }
-  
-    .popup {
-      background-color: #fff;
-      padding: 20px;
-      border-radius: 5px;
-    }
-    .contenuPage{
-        margin-left: 120px;
-    }
-    .messageActuel{
-        border-left: 1px solid #dddddd;
-    }
-    .titrePage{
-        background-color: rgb(62, 62, 155);
-        color: aliceblue;
-    }
 
-</style>
